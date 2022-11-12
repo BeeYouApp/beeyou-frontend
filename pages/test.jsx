657116383c46faa0012e9cde69f2b248251bcbbd
abyblackmouth <abraham.covarrubias@live.com>
2022-11-12 11:58:23 -0800

    started project

---
+++ pages/test.jsx
@@ -0,0 +1,10 @@
+
+export default function Test() {
+  return (
+      <main className="text-3xl font-bold underline">
+        <h1 className="text-3xl font-bold underline text-red-600">
+          Hola a todos
+        </h1>
+      </main>
+  )
+}
