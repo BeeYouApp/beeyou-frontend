ce7f409eb38c9d7c24159b996bce9d0083f6ede4
abyblackmouth <abraham.covarrubias@live.com>
2022-11-19 19:41:40 -0600

    add button nav mobile

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -11,18 +11,24 @@ export default function Navbar(){
         'flex justify-between fixed top-0',
         'z-40',
         'w-full',
-        'space-x-96  ')}>
-        <div className={clsx('flex items-center pl-6')}>
+        'space-x-10 sppace-y-4  ')}>
+        <div className={clsx('flex items-center pl-6 ')}>
           <Link href=''>
             <img src="/logo-beeyou.svg" alt="Logo" />
           </Link>
         </div>
           
-        <div className="flex justify-end items-center text-neutral-700 font-bold ">
+        <div className="flex items-center text-neutral-700 font-bold max-lg:hidden">
           <a className="m-6" href="">Inicio</a>
           <a className="m-6" href="">Beneficios</a>
           <a className="m-6" href="">Iniciar sesion</a>
           <button className="m-6 p-2 rounded-lg border-4 border-rose-500 ">Unete a Bee you+!</button>  
+        </div>
+        
+        <div className={clsx("px-4 cursor-pointer flex justify-end items-center lg:hidden")}>
+          <Link href=''>
+            <img src="/icon-menu-mobile.svg" alt="menu" />
+          </Link>  
         </div>
       </nav>  
     </>
