5da3e8018345e2f9a54f350ebcc683debffb024c
Víctor Cosme <victorcosmema@gmail.com>
2022-11-21 10:42:50 -0600

    Add LandingPlang section

--- lib/images.jsx
+++ lib/images.jsx
@@ -1,10 +1,11 @@
 import medalIcon from "../public/assets/landing/medal.webp";
 import checkIcon from "../public/assets/landing/check.webp";
 import proIcon from "../public/assets/landing/pro.webp";
+import freeIcon from "../public/assets/landing/free.webp";
 
 export const images = {
-    medal: medalIcon,
-    check: checkIcon,
-    pro: proIcon
-
-}
+  medal: medalIcon,
+  check: checkIcon,
+  pro: proIcon,
+  free: freeIcon,
+};
--- tailwind.config.js
+++ tailwind.config.js
@@ -9,7 +9,7 @@ module.exports = {
     extend: {
       fontFamily: {
         montserrat: ["Montserrat", "sans-serif"],
-        poppins: ["Poppins", "sans-serif"]
+        poppins: ["Poppins", "sans-serif"],
       },
       colors: {
         "blue-gray": {
@@ -24,18 +24,18 @@ module.exports = {
           100: "#CFD8DC",
           50: "#ECEFF1",
         },
-        "purple": {
+        purple: {
           700: "#FF68FF",
           800: "#CA2CE4",
           900: "#9500B1",
         },
-        "yellow": {
+        yellow: {
           700: "#EFD16B",
         },
-        "green": {
+        green: {
           900: "#71D03C",
         },
-        "madang": {
+        madang: {
           50: "#B9FF92",
         },
         "red-gradient": "#FF4C35",
@@ -43,7 +43,7 @@ module.exports = {
         "yellow-gradient": "#FBC600",
         "green-gradient": "#72C63D",
         "purple-gradient": "#665DFB",
-        "pink-gradient": "#C92CE5"
+        "pink-gradient": "#C92CE5",
       },
     },
   },
@@ -51,5 +51,5 @@ module.exports = {
 };
 
 // "gradient-lgbt":
-        // "bg-gradient-to-r from-[#FF4C35]/7.46, via-[#FE9B05]/22.91 via-[#FBC600]/38.35 via-[#72C63D]/57.377 via-[#665DFB]/73.66 to-[#C92CE5/92.2]",
-        // "linear-gradient(90.41deg, #FF4C35 7.46%, #FE9B05 22.91%, #FBC600 38.35%, #72C63D 57.77%, #665DFB 73.66%, #C92CE5 92.2%);",
+// "bg-gradient-to-r from-[#FF4C35]/7.46, via-[#FE9B05]/22.91 via-[#FBC600]/38.35 via-[#72C63D]/57.377 via-[#665DFB]/73.66 to-[#C92CE5/92.2]",
+// "linear-gradient(90.41deg, #FF4C35 7.46%, #FE9B05 22.91%, #FBC600 38.35%, #72C63D 57.77%, #665DFB 73.66%, #C92CE5 92.2%);",
