b28f3b5b7ead485e8ad439fe33d8d336f160818b
annie <any_prettier_3@hotmail.com>
2022-12-01 18:46:47 -0600

    adding complete sidebar

---
+++ components/LayoutDashboard.jsx
@@ -0,0 +1,21 @@
+import clsx from "clsx";
+import Sidebar from "./SideBar";
+
+export default function LayautDashboard({children, style}){
+  return(
+    <>
+      <aside className=" inline-flex h-screen w-1/6 border-2">
+        <Sidebar></Sidebar>
+      </aside>
+      <main className={clsx(
+        "inline-block", 
+        "h-screen",
+        "w-5/6",
+        "absolute",
+        {style}
+        )}>
+        {children}
+      </main>
+    </>
+   )
+}
