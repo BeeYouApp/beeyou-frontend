cae524d87936fdac2fb2657c6b8c03a5a04fea06
abyblackmouth <abraham.covarrubias@live.com>
2022-12-01 18:33:22 -0600

    deleted Sidebar componet

--- components/LayoutDashboard.jsx
+++ components/LayoutDashboard.jsx
@@ -1,12 +1,10 @@
 import clsx from "clsx";
-import Sidebar from "./Sidebar";
 import SearchMap from "./SearchMap";
 
 export default function LayautDashboard({children, style}){
   return(
     <>
       <aside className=" inline-flex h-screen w-1/6 bg-red-400">
-        <Sidebar></Sidebar>
       </aside>
       <main className={clsx(
         "inline-block", 
