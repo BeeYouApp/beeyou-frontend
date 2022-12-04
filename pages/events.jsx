6a926b8dc310239af413b419da4d3115ab83eae6
Víctor Cosme <victorcosmema@gmail.com>
2022-12-04 14:12:50 -0600

    Add sidebar test and clsx classes to events page

--- pages/events.jsx
+++ pages/events.jsx
@@ -8,76 +8,82 @@ import TableEvents from "../components/TableEvents";
 export default function Events() {
   return (
     <section>
-      <Navbar />
-      <article className={clsx("px-4 md:px-8 mt-24")}>
-        <h3 className={clsx("font-montserrat font-bold text-blue-gray-900 text-[24px] leading-[30px]")}>
-          Tus eventos
-        </h3>
-        <section className={clsx("flex items-center mt-4 mb-5")}>
-          <Image
-            className={clsx("")}
-            src={images.nearEventIcon}
-            alt="Near Event Icon"
-          />
-          <h4 className={clsx("font-montserrat font-normal text-blue-gray-700 text-[20px] leading-[28px] ml-6")}>
-            Tus eventos cercanos
-          </h4>
-        </section>
-        <section className={clsx("md:flex md:justify-evenly")}>
-          <div className={clsx("flex justify-center")}>
-            <section className={clsx("hidden md:flex md:items-center md:mr-[65px]")}>
+      <div className={clsx("block lg:hidden")}>
+        <Navbar />
+      </div>
+      <div className={clsx("flex")}>
+        <article>
+          <div className={clsx("w-[247px] h-[745px] bg-blue-gray-100 hidden lg:block")}></div>
+        </article>
+        <article className={clsx("px-4 md:px-8 mt-24 lg:mt-14")}>
+          <h3 className={clsx("font-montserrat font-bold text-blue-gray-900 text-[24px] leading-[30px]")}>
+            Tus eventos
+          </h3>
+          <section className={clsx("flex items-center mt-4 mb-5")}>
+            <Image
+              className={clsx("")}
+              src={images.nearEventIcon}
+              alt="Near Event Icon"
+            />
+            <h4 className={clsx("font-montserrat font-normal text-blue-gray-700 text-[20px] leading-[28px] ml-6")}>
+              Tus eventos cercanos
+            </h4>
+          </section>
+          <section className={clsx("md:flex md:justify-evenly")}>
+            <div className={clsx("flex justify-center")}>
+              <section className={clsx("hidden md:flex md:items-center md:mr-[65px]")}>
+                <Image
+                  className={clsx("")}
+                  src={images.leftArrow}
+                  alt="Left Arrow Icon"
+                />
+              </section>
+              <CardEventsSm />
+            </div>
+            <div className={clsx("hidden md:block")}>
+              <CardEventsSm />
+            </div>
+            <div className={clsx("hidden lg:block")}>
+              <CardEventsSm />
+            </div>
+            <div className={clsx("hidden lg:block")}>
+              <CardEventsSm />
+            </div>
+            <section className={clsx("hidden md:flex md:items-center md:ml-[65px]")}>
+              <Image
+                className={clsx("")}
+                src={images.rightArrow}
+                alt="Right Arrow Icon"
+              />
+            </section>
+          </section>
+          <section className={clsx("md:hidden")}>
+            <section className={clsx("flex justify-end mt-6")}>
               <Image
                 className={clsx("")}
                 src={images.leftArrow}
                 alt="Left Arrow Icon"
               />
+              <Image
+                className={clsx("ml-10")}
+                src={images.rightArrow}
+                alt="Right Arrow Icon"
+              />
             </section>
-            <CardEventsSm />
-          </div>
-          <div className={clsx("hidden md:block")}>
-            <CardEventsSm />
-          </div>
-          <div className={clsx("hidden lg:block")}>
-            <CardEventsSm />
-          </div>
-          <div className={clsx("hidden lg:block")}>
-            <CardEventsSm />
-          </div>
-          <section className={clsx("hidden md:flex md:items-center md:ml-[65px]")}>
-            <Image
-              className={clsx("")}
-              src={images.rightArrow}
-              alt="Right Arrow Icon"
-            />
           </section>
-        </section>
-        <section className={clsx("md:hidden")}>
-          <section className={clsx("flex justify-end mt-6")}>
-            <Image
-              className={clsx("")}
-              src={images.leftArrow}
-              alt="Left Arrow Icon"
-            />
-            <Image
-              className={clsx("ml-10")}
-              src={images.rightArrow}
-              alt="Right Arrow Icon"
-            />
+          <section>
+            <section className={clsx("flex items-center mt-10 mb-6")}>
+              <Image src={images.allEventsIcon} alt="All Events Icon" />
+              <h4 className={clsx("font-montserrat font-normal text-blue-gray-700 text-[20px] leading-[28px] ml-6")}>
+                Todos tus eventos
+              </h4>
+            </section>
+            <div className={clsx("flex flex-col")}>
+              <TableEvents />
+            </div>
           </section>
-        </section>
-        <section>
-          <section className={clsx("flex items-center mt-10 mb-6")}>
-            <Image src={images.allEventsIcon} alt="All Events Icon" />
-            <h4
-              className={clsx("font-montserrat font-normal text-blue-gray-700 text-[20px] leading-[28px] ml-6")}>
-              Todos tus eventos
-            </h4>
-          </section>
-          <div className={clsx("flex flex-col")}>
-          <TableEvents />            
-          </div>
-        </section>
-      </article>
+        </article>
+      </div>
     </section>
   );
 }
