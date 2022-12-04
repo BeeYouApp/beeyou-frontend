2728949e75323d2c1cc77b97a2a276a3f4872d3a
Víctor Cosme <victorcosmema@gmail.com>
2022-12-04 11:28:49 -0600

    Delete cards page & Format CardPlacesSm

--- components/CardPlacesSm.jsx
+++ components/CardPlacesSm.jsx
@@ -13,22 +13,12 @@ export default function CardPlacesSm(props) {
             alt="Place Image"
           />
         </a>
-        <div
-          className={clsx(
-            "font-montserrat absolute bottom-0 left-0 right-0 px-4 py-2 text-montserrat text-[#FFFFFF]"
-          )}
-        >
-          <h5
-            className={clsx("text-[14px] font-bold leading-[15px] mt-[72px]")}
-          >
+        <div className={clsx("font-montserrat absolute bottom-0 left-0 right-0 px-4 py-2 text-montserrat text-[#FFFFFF]")}>
+          <h5 className={clsx("text-[14px] font-bold leading-[15px] mt-[72px]")}>
             Cielito Querido
           </h5>
           <span className={clsx("mt-1.5")}>☆☆☆☆☆</span>
-          <p
-            className={clsx(
-              "flex text-[10px] font-light leading-[12px] aling-items self-end"
-            )}
-          >
+          <p className={clsx("flex text-[10px] font-light leading-[12px] aling-items self-end")}>
             <span className="self-center mr-1 leading-[12px]">
               100% FRIENDLY
             </span>
