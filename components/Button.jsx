36d3212ac478ada50faae29f64b15c989ce33a01
abyblackmouth <abraham.covarrubias@live.com>
2022-12-05 01:18:52 -0600

    Profile configuration page components are created

--- components/Button.jsx
+++ components/Button.jsx
@@ -8,8 +8,7 @@ export default function Button({ label, style, onClick, isSubmit = false }) {
           className={clsx(
             // 'bg-sky-600', // Pending to transform to 'props'
             "py-2 px-4",
-            "rounded",
-            "text-white text-center font-bold",
+            "text-white text-center font-bold font-montserrat",
             "w-40",
             "hover:bg-sky-900",
             "focus:outline-none focus:shadow-outline",
@@ -25,8 +24,7 @@ export default function Button({ label, style, onClick, isSubmit = false }) {
           className={clsx(
             // 'bg-lgtb bg-cover', // pendiente tranformarlo a props
             "py-2 px-4",
-            "rounded-lg",
-            "text-white text-center font-bold",
+            "text-white text-center font-bold font-montserrat",
             "w-[300px] h-[50px]",
             "hover:bg-gradient-to-r from-teal-400 to-[#249F95]/80",
             "focus:outline-none focus:shadow-outline",
--- components/CardEventsSm.jsx
+++ components/CardEventsSm.jsx
@@ -4,15 +4,14 @@ import Image from "next/image";
 import { images } from "../lib/images";
 
 export default function CardEventsSm () {
-    return (
-            <div className={clsx("shadow-lg w-fit md:max-w-sm md:mr-5 md:ml-5")}>
-              <a href="#!">
-                <Image
-                  className={clsx("rounded-lg w-[328px] md:w-[215px] h-[257px] object-none")}
-                  src={images.paradeFlag}
-                  alt="Parade Flag Image"
-                />
-              </a>
-            </div>
+  return (
+    <div className={clsx("shadow-lg w-fit md:max-w-sm md:mr-5 md:ml-5")}>
+      <a href="#!">
+        <Image
+          className={clsx("rounded-lg w-[328px] md:w-[215px] h-[257px] object-none")}
+          src={images.paradeFlag}
+          alt="Parade Flag Image"/>
+      </a>
+    </div>
     )
 }
