7996edfa32daf4485bcca123aa41622b0c3fc167
abyblackmouth <abraham.covarrubias@live.com>
2022-12-08 02:03:15 -0600

    add bussines Form created fixed AutoFillMap component

---
+++ components/BussinesForm.jsx
@@ -0,0 +1,178 @@
+import clsx from "clsx"
+import React from "react"
+import {useForm} from "react-hook-form"
+import Input from "./Input"
+import { ToastContainer, toast } from "react-toastify"
+import Button from "./Button"
+import dynamic from 'next/dynamic'
+import Map from "./Map"
+
+const DynamicComponent = dynamic(() =>
+  import('../components/AuitofillMap'), {
+    ssr: false,
+  }
+)
+
+export default function BussinesForm(){
+  const {register,handleSubmit} = useForm()
+  const onSubmit = async data => {
+  {console.log(data)}
+  const result = await register(data)
+  console.log("result:", result)
+  if(!result ){
+    toast.error("ups hubo un error")
+  } 
+
+}
+  return(
+    <section className="w-[680px] h-[362px] max-lg:w-[340px] max-lg:h-[1560px] rounded-[16px] border-2 border-blue-sky-50 flex flex-col shadow-xl absolute p-5 mr-10 max-lg:mr-[0px]">
+      <article>
+        <h1 className="mt-[13px] max-lg:mt-[20px] text-[20px] font-bold font-montserrat text-blue-gray-900 ml-4  max-lg:ml-[3px]">Datos del negocio</h1>
+        <h3 className="mt-[15px] mb-[8px] text-[16px] font-medium font-montserrat text-blue-gray-900 ml-4 max-lg:ml-[3px]">Nombre comercial</h3>
+      </article>
+    <section className="inline-flex max-lg:inline-block">
+
+      <article className=" m-auto">
+        <Input
+          htmlFor='name' 
+          label='NOMBRE COMERCIAL DE TU NEGOCIO'
+          id='username'
+          type='string'
+          placeholder='Ingresa nombre de tu negocio*'
+          message='error'
+          register={register}
+          style="">
+        </Input>
+        <Input
+          htmlFor='name' 
+          label='RFC'
+          id='username'
+          type='string'
+          placeholder='Ingresa RFC*'
+          message='error'
+          register={register}
+          style="">
+        </Input>
+      </article>
+
+
+        <h3 className="mt-[15px] mb-[8px] text-[16px] font-medium font-montserrat text-blue-gray-900 ml-4 max-lg:ml-[3px]">Persona Moral</h3>
+      <article className="">
+        <Input
+          htmlFor='name' 
+          label='NOMBRE Y APELLIDO DEL REPRESENTANTE LEGAL DEL NEGOCIO'
+          id='username'
+          type='string'
+          placeholder='ingresar dato*'
+          message='error'
+          register={register}
+          style="">
+        </Input>
+        <Input
+          htmlFor='name' 
+          label='GIRO DEL NEGOCIO'
+          id='username'
+          type='string'
+          placeholder='Giro de tu negocio*'
+          message='error'
+          register={register}
+          style="">
+        </Input>
+      </article>
+      <article>
+      <label className={clsx(
+        'text-[12px] font-montserrat font-medium text-blue-gray-500',
+        'block ml-1 mt-2',
+        )}> DESCRIPCION DE TU NEGOCIO
+      </label>
+        <textarea className={clsx(
+            'shadow w-full h-[100px]' ,
+            'mt-[12px]',
+            'appearance-none',
+            'border w-[300px] h-[40px] rounded-lg py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline',
+            'hover:border-violet-700 border-2 mb-2',
+        )}></textarea>
+      </article>
+      
+      <article>
+        <label className={clsx(
+          'text-[12px] font-montserrat font-medium text-blue-gray-500',
+          'block ml-1 mt-2',
+          )}> HORARIO DE SERVICIO
+        </label>
+          <div className="inline-flex">
+            <Input
+              htmlFor='name' 
+              label=''
+              id='username'
+              type='time'
+              placeholder='Giro de tu negocio*'
+              message='error'
+              register={register}
+              style="w-[139px]">
+            </Input>
+              <h3 className="m-auto ml-1 text-[12px] font-montserrat font-bold text-blue-gray-900">A:</h3>
+            <Input
+              htmlFor='name' 
+              label=''
+              id='username'
+              type='time'
+              placeholder='Giro de tu negocio*'
+              message='error'
+              register={register}
+              style="w-[139px]">
+            </Input>
+          </div>
+      </article>
+
+      <article>
+        <label className={clsx(
+          'text-[12px] font-montserrat font-medium text-blue-gray-500',
+          'block ml-1 mt-3 mb-2',
+          )}> DIAS DE SERVICIO
+        </label>
+        <form className="text-gray-700 font-montserrat" onSubmit={handleSubmit(console.log)}>
+          <section className="flex justify-between">
+            <input {...register("checkbox")} type="checkbox" value="A" />Lunes
+            <input {...register("checkbox")} type="checkbox" value="B" />Martes
+            <input {...register("checkbox")} type="checkbox" value="C" />Miercoles
+          </section>
+          <section className="mt-1 flex justify-between">
+            <input {...register("checkbox")} type="checkbox" value="D" />Jueves
+            <input {...register("checkbox")} type="checkbox" value="E" />Viernes
+            <input {...register("checkbox")} type="checkbox" value="F" />Sabado
+          </section>
+          <section className="mt-1 flex justify-between">
+          <input {...register("checkbox")} type="checkbox" value="G" />Domingo
+          <input {...register("checkbox")} type="checkbox" value="G" />Todos los dias
+          </section>
+{/*          <input type="submit" /> */}
+       </form>
+      </article>
+        <label className={clsx(
+          'text-[12px] font-montserrat font-medium text-blue-gray-500',
+          'block ml-1 mt-3 mb-2',
+          )}> DIRECCION DE TU NEGOCIO
+        </label>
+        <DynamicComponent></DynamicComponent>
+      <article>
+
+      </article>
+
+    </section>
+    <article className=" mt-3 flex justify-end">
+        <Button
+          label='CONTUNUAR'
+          isSubmit
+          style={clsx(
+            "lgbtiq-grad-bg rounded-lg  max-lg:mt-[10px]"
+          )}>
+        </Button>
+    </article>
+    <article className="w-full h-[50px] relative mt-4">
+      <Map></Map>
+    </article>
+  
+  </section>
+    )
+}
