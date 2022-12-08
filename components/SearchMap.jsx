c0c47d7e5a708d3d841c77056fe9c8f1b282879e
abyblackmouth <abraham.covarrubias@live.com>
2022-12-07 20:52:23 -0600

    add map search component

--- components/SearchMap.jsx
+++ components/SearchMap.jsx
@@ -1,63 +1,54 @@
-/* import "mapbox-gl/dist/mapbox-gl.css"
-import 'react-map-gl-geocoder/dist/mapbox-gl-geocoder.css'
-import React, { useState, useRef, useCallback } from "react"
-import MapGL from "react-map-gl"
-import Geocoder from "react-map-gl-geocoder"
+import clsx from 'clsx';
+import React, { useState, useCallback, useEffect } from 'react';
+import { AddressAutofill} from '@mapbox/search-js-react';
 
-// Please be a decent human and don't abuse my Mapbox API token.
-// If you fork this sandbox, replace my API token with your own.
-// Ways to set Mapbox token: https://uber.github.io/react-map-gl/#/Documentation/getting-started/about-mapbox-tokens
-const MAPBOX_TOKEN ='pk.eyJ1IjoiYWJ5YmxhY2ttb3V0aCIsImEiOiJjbGFodTBobmowODIwM3hvYmxva20zYWQ4In0.GWgmsYW9P5xUKzxDGliiVg';
-
-
-export default function SearchMap(){
-
-  const [viewport, setViewport] = useState({
-    latitude: 37.7577,
-    longitude: -122.4376,
-    zoom: 8
-  });
-  const mapRef = useRef();
-  const handleViewportChange = useCallback(
-    (newViewport) => setViewport(newViewport),
-    []
+export default function SearchMap () {
+  return(
+    <form className='font-montserrat font-medium'>
+      <AddressAutofill accessToken="sk.eyJ1IjoiYWJ5YmxhY2ttb3V0aCIsImEiOiJjbGJkN29mMmUwemFlM29vNXcyeDlvaGVyIn0.SO4z3m0gdj7Pe2Rlu_bE9A">
+        <label className='mr-3'>Direccion</label>
+        <input
+            className='border'
+            name="address" 
+            placeholder="Adress" 
+            type="text"
+            autoComplete="address-line1"/>
+      </AddressAutofill>
+      <label className='mr-3 ml-3'>Apart</label>
+        <input
+        className='border'
+            name="apartment" 
+            placeholder="Apartamento" 
+            type="text"
+            autoComplete="address-line2"/>
+        <label className='mr-3 ml-3'>Ciudad</label>
+        <input
+            className='border'
+            name="city" 
+            placeholder="Ciudad" 
+            type="text"
+            autoComplete="address-level2"/>
+        <label className='mr-3 ml-3'>Estado</label>
+        <input
+            className='border'
+            name="state" 
+            placeholder="Estado" 
+            type="text"
+            autoComplete="address-level1"/>
+        <label className='mr-3 ml-3'>Pais</label>
+        <input
+            className='border'
+            name="country" 
+            placeholder="Pais" 
+            type="text"
+            autoComplete="country"/>
+        <label className='mr-3 ml-3'>C.P.</label>
+        <input
+            className='border'
+            name="postcode" 
+            placeholder="Codigo Postal" 
+            type="text"
+            autoComplete="postal-code"/>
+    </form>
   )
-  
-  // si está satisfecho con la configuración predeterminada de Geocoder, puede usar handleViewportChange directamente
-
-  const handleGeocoderViewportChange = useCallback(
-    (newViewport) => {
-      const geocoderDefaultOverrides = { transitionDuration: 1000 };
-
-      return handleViewportChange({
-        ...newViewport,
-        ...geocoderDefaultOverrides
-      });
-    },
-    [handleViewportChange]
-  );
-
-  return (
-    <div style={{ height: "100vh" }}>
-      <MapGL
-        ref={mapRef}
-        {...viewport}
-        width="100%"
-        height="100%"
-        onViewportChange={handleViewportChange}
-        mapboxApiAccessToken={MAPBOX_TOKEN}
-      >
-        <Geocoder
-          mapRef={mapRef}
-          onViewportChange={handleGeocoderViewportChange}
-          mapboxApiAccessToken={MAPBOX_TOKEN}
-          position="top-left"
-        />
-      </MapGL>
-    </div>
-  );
-};
- */
-
-/* render(<App />, document.getElementById("root")); */
-
+}
