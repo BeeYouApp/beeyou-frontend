8ec6b6c04b4e765fde65c165733d39a9ffa73843
annie <any_prettier_3@hotmail.com>
2022-12-09 13:08:20 -0600

    update profile

--- pages/updateProfile.jsx
+++ pages/updateProfile.jsx
@@ -7,26 +7,28 @@ export default function UpdateProf(){
   return(
     <>
       <Navbar/>
-        <main className=" w-screen h-screen p-10 ">
-          <div className="flex">
-            <Sidebar></Sidebar>
-            <section className="inline-block w-full h-1/4 m-auto font-poppins max-lg:h-[250px]">
-              <article className="text-center text-3xl font-bold mb-4">
-                <h1 className="mt-20 text-blue-gray-900 font-poppins ">Actualicemos tu perfil</h1>
-              </article>
-            </section>
-            <article className="inline-flex w-3/12 max-lg:w-full h-1/4 max-lg:h-[450px] justify-end pt-[32px] max-lg:flex max-lg:justify-center">
-              <CardProfile></CardProfile>
-            </article>
-            <article className="inline-flex w-4/12 max-lg:w-full h-1/4 3 max-lg:h-[620px] pl-[40px] pt-[32px] max-lg:flex max-lg:justify-center max-lg:pl-[0px] ">
-              <UpdateProfile></UpdateProfile>
-              </article>
-            <article className="inline-flex w-4/12 max-lg:w-full h-2/4 3 max-lg:h-[620px] pl-[40px] pt-[32px] max-lg:flex max-lg:justify-center max-lg:pl-[0px] ">
-              <UpdateProfile></UpdateProfile>
-            </article>
-          </div>
-        </main>
-        </>
-    )
+      <div className="flex">
+      <Sidebar/>
+      <article className="w-1/4 px-4 md:px-8 mt-24 lg:mt-14">
+        <div className="flex lg:mt-10 xl:mt-0">
+          <h1 className="font-montserrat font-bold text-blue-gray-900 text-[24px] leading-[30px] mr-[79px]">Actualicemos tu perfil</h1>
+        </div>
+        <section>
+          <article className="inline-flex w-1/4 max-lg:w-full h-2/4 max-lg:h-[450px] justify-end pt-[32px] max-lg:flex max-lg:justify-center">
+            <CardProfile></CardProfile>
+          </article>
+          <article className="inline-flex w-2/4 max-lg:w-full h-2/4 3 max-lg:h-[620px] pl-[40px] pt-[32px] max-lg:flex max-lg:justify-center max-lg:pl-[0px] ">
+            <UpdateProfile></UpdateProfile>
+          </article>        
+        </section>
+        <section>
+          <article className="k w-2/4 max-lg:w-full h-4/4 3 max-lg:h-[620px] pl-[40px] pt-[32px] max-lg:flex max-lg:justify-center max-lg:pl-[0px] ">
+            <UpdateProfile></UpdateProfile>
+          </article>
+        </section>
+      </article>
+      </div>
+    </>
+  )
 
 }
