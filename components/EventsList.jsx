036c597a9f03e724b86ed21e79fe35aa48e53ad4
annie <any_prettier_3@hotmail.com>
2022-12-10 18:11:55 -0600

    create events list page

--- components/EventsList.jsx
+++ components/EventsList.jsx
@@ -2,87 +2,188 @@ import clsx from "clsx";
 import Link from "next/link";
 import Image from "next/image";
 import { images } from "../lib/images";
-import TableFavoritesRow from "../components/TableFavoritesRow";
+import TableEventsRow from "../components/TableEventsRow";
 
 export default function TableEvents() {
   return (
-    <div className={clsx("flex justify-center overflow-x-auto sm:-mx-6 lg:-mx-8")}>
-      <div className={clsx("py-4 md:mb-[75px] inline-block justify-center min-w-[1050px] sm:px-6 lg:px-8")}>
-        <div className={clsx("overflow-y-auto")}>
+    <div className={clsx("flex justify-center sm:-mx-8 lg:-mx-8")}>
+      <div className={clsx("bg-yellow-100 py-10 md:mb-[75px] inline-block justify-center max-h-[600px] max-w-[328px] md:min-w-[680px] lg:min-w-[1050px] 2xl:min-w-[2166px] 2xl:max-h-[700px] sm:px-6 lg:px-8")}>
+        <div className={clsx("bg-red-100")}>
+          <button
+              className={clsx(
+                "purple-button bg-purple-900 border-purple-900 text-[#FFFFFF] md:w-[194px] h-[48px] rounded-[8px] justify-items-end m-10"
+              )}
+            >
+              <h4>+ Nuevo evento</h4>
+              {/* {props.children} */}
+            </button>
           <table className={clsx("min-w-full text-center")}>
             <thead className={clsx("border-b bg-gray-50")}>
               <tr>
-                <th scope="col" className={clsx("text-start text-[14px] font-medium text-blue-gray-600 pl-6 pr-32 py-4")}>
-                  PROMOCIÓN
+                <th scope="col" className={clsx("min-w-full text-start text-[14px] font-medium text-blue-gray-600 pl-6 pr-32 py-4")}>
+                  EVENTO
                 </th>
                 <th scope="col" className={clsx("text-[14px] font-medium text-blue-gray-600 px-6 py-4")}>
-                  DESCRIPCIÓN
+                  TIPO/TEMÁTICA
                 </th>
                 <th scope="col" className={clsx("text-[14px] font-medium text-blue-gray-600 px-6 py-4")}>
-                  INICIA
+                  FECHA
                 </th>
                 <th scope="col" className={clsx("hidden lg:block text-[14px] font-medium text-blue-gray-600 px-6 py-4")}>
-                  FINALIZA
+                  HORARIO
+                </th>
+                <th scope="col" className={clsx("text-[14px] font-medium text-blue-gray-600 px-6 py-4")}>
+                  COSTO
+                </th>
+                <th scope="col" className={clsx("text-[14px] font-medium text-blue-gray-600 px-6 py-4")}>
+                  AFORO
+                </th>
+                <th scope="col" className={clsx("text-[14px] font-medium text-blue-gray-600 px-6 py-4")}>
+                  MÁS
                 </th>
               </tr>
             </thead>
             <tbody>
-              <TableFavoritesRow>
+              <TableEventsRow>
                 <td className={clsx("flex items-center text-[14px] text-blue-gray-600 font-normal px-6 py-4 whitespace-nowrap")}>
-                  10% de descuento en todas las bebidas
-                </td>
-                <td className={clsx("text-[14px] text-blue-gray-600 font-normal px-6 py-4 whitespace-nowrap")}>
+                  <Image
+                    className={clsx("w-16 h-12 mr-3")}
+                    src={images.beeYouEvent}
+                    alt="Near Event Icon"
+                  />
                   Orgullo 2023
                 </td>
                 <td className={clsx("text-[14px] text-blue-gray-600 font-normal px-6 py-4 whitespace-nowrap")}>
-                  16/12/2022
-                </td>
-                <td className={clsx("hidden lg:table-cell text-[14px] text-blue-gray-600 font-normal px-6 py-4 whitespace-nowrap")}>
-                  07:00 pm
-                </td>
-              </TableFavoritesRow>
-              <TableFavoritesRow>
-                <td className={clsx("flex items-center text-[14px] text-blue-gray-600 font-normal px-6 py-4 whitespace-nowrap")}>
-                  10% de descuento en todas las bebidas
+                  Un evento para unir a la comunidad
                 </td>
                 <td className={clsx("text-[14px] text-blue-gray-600 font-normal px-6 py-4 whitespace-nowrap")}>
+                  17/01/2023
+                </td>
+                <td className={clsx("hidden lg:table-cell text-[14px] text-blue-gray-600 font-normal px-6 py-4 whitespace-nowrap")}>
+                  13:00
+                </td>
+                <td className={clsx("text-[14px] text-blue-gray-600 font-normal px-6 py-4 whitespace-nowrap")}>
+                  U$125.00
+                </td>
+                <td className={clsx("text-[14px] text-blue-gray-600 font-normal px-6 py-4 whitespace-nowrap")}>
+                  200
+                </td>
+                <td className={clsx("text-[14px] text-blue-gray-600 font-normal px-6 py-4 whitespace-nowrap")}>
+                  ...
+                </td>
+              </TableEventsRow>
+              <TableEventsRow>
+                <td className={clsx("flex items-center text-[14px] text-blue-gray-600 font-normal px-6 py-4 whitespace-nowrap")}>
+                  <Image
+                    className={clsx("w-16 h-12 mr-3")}
+                    src={images.beeYouEvent}
+                    alt="Near Event Icon"
+                  />
                   Orgullo 2023
                 </td>
                 <td className={clsx("text-[14px] text-blue-gray-600 font-normal px-6 py-4 whitespace-nowrap")}>
-                  16/12/2022
-                </td>
-                <td className={clsx("hidden lg:table-cell text-[14px] text-blue-gray-600 font-normal px-6 py-4 whitespace-nowrap")}>
-                  07:00 pm
-                </td>
-              </TableFavoritesRow>
-              <TableFavoritesRow>
-                <td className={clsx("flex items-center text-[14px] text-blue-gray-600 font-normal px-6 py-4 whitespace-nowrap")}>
-                  10% de descuento en todas las bebidas
+                  Un evento para unir a la comunidad
                 </td>
                 <td className={clsx("text-[14px] text-blue-gray-600 font-normal px-6 py-4 whitespace-nowrap")}>
+                  17/01/2023
+                </td>
+                <td className={clsx("hidden lg:table-cell text-[14px] text-blue-gray-600 font-normal px-6 py-4 whitespace-nowrap")}>
+                  13:00
+                </td>
+                <td className={clsx("text-[14px] text-blue-gray-600 font-normal px-6 py-4 whitespace-nowrap")}>
+                  U$125.00
+                </td>
+                <td className={clsx("text-[14px] text-blue-gray-600 font-normal px-6 py-4 whitespace-nowrap")}>
+                  200
+                </td>
+                <td className={clsx("text-[14px] text-blue-gray-600 font-normal px-6 py-4 whitespace-nowrap")}>
+                  ...
+                </td>
+              </TableEventsRow>
+              <TableEventsRow>
+                <td className={clsx("flex items-center text-[14px] text-blue-gray-600 font-normal px-6 py-4 whitespace-nowrap")}>
+                  <Image
+                    className={clsx("w-16 h-12 mr-3")}
+                    src={images.beeYouEvent}
+                    alt="Near Event Icon"
+                  />
                   Orgullo 2023
                 </td>
                 <td className={clsx("text-[14px] text-blue-gray-600 font-normal px-6 py-4 whitespace-nowrap")}>
-                  16/12/2022
-                </td>
-                <td className={clsx("hidden lg:table-cell text-[14px] text-blue-gray-600 font-normal px-6 py-4 whitespace-nowrap")}>
-                  07:00 pm
-                </td>
-              </TableFavoritesRow>
-              <TableFavoritesRow>
-                <td className={clsx("flex items-center text-[14px] text-blue-gray-600 font-normal px-6 py-4 whitespace-nowrap")}>
-                  10% de descuento en todas las bebidas
+                  Un evento para unir a la comunidad
                 </td>
                 <td className={clsx("text-[14px] text-blue-gray-600 font-normal px-6 py-4 whitespace-nowrap")}>
+                  17/01/2023
+                </td>
+                <td className={clsx("hidden lg:table-cell text-[14px] text-blue-gray-600 font-normal px-6 py-4 whitespace-nowrap")}>
+                  13:00
+                </td>
+                <td className={clsx("text-[14px] text-blue-gray-600 font-normal px-6 py-4 whitespace-nowrap")}>
+                  U$125.00
+                </td>
+                <td className={clsx("text-[14px] text-blue-gray-600 font-normal px-6 py-4 whitespace-nowrap")}>
+                  200
+                </td>
+                <td className={clsx("text-[14px] text-blue-gray-600 font-normal px-6 py-4 whitespace-nowrap")}>
+                  ...
+                </td>
+              </TableEventsRow>
+              <TableEventsRow>
+                <td className={clsx("flex items-center text-[14px] text-blue-gray-600 font-normal px-6 py-4 whitespace-nowrap")}>
+                  <Image
+                    className={clsx("w-16 h-12 mr-3")}
+                    src={images.beeYouEvent}
+                    alt="Near Event Icon"
+                  />
                   Orgullo 2023
                 </td>
                 <td className={clsx("text-[14px] text-blue-gray-600 font-normal px-6 py-4 whitespace-nowrap")}>
-                  16/12/2022
+                  Un evento para unir a la comunidad
+                </td>
+                <td className={clsx("text-[14px] text-blue-gray-600 font-normal px-6 py-4 whitespace-nowrap")}>
+                  17/01/2023
                 </td>
                 <td className={clsx("hidden lg:table-cell text-[14px] text-blue-gray-600 font-normal px-6 py-4 whitespace-nowrap")}>
-                  07:00 pm
+                  13:00
                 </td>
-              </TableFavoritesRow>
+                <td className={clsx("text-[14px] text-blue-gray-600 font-normal px-6 py-4 whitespace-nowrap")}>
+                  U$125.00
+                </td>
+                <td className={clsx("text-[14px] text-blue-gray-600 font-normal px-6 py-4 whitespace-nowrap")}>
+                  200
+                </td>
+                <td className={clsx("text-[14px] text-blue-gray-600 font-normal px-6 py-4 whitespace-nowrap")}>
+                  ...
+                </td>
+              </TableEventsRow>
+              <TableEventsRow>
+                <td className={clsx("flex items-center text-[14px] text-blue-gray-600 font-normal px-6 py-4 whitespace-nowrap")}>
+                  <Image
+                    className={clsx("w-16 h-12 mr-3")}
+                    src={images.beeYouEvent}
+                    alt="Near Event Icon"
+                  />
+                  Orgullo 2023
+                </td>
+                <td className={clsx("text-[14px] text-blue-gray-600 font-normal px-6 py-4 whitespace-nowrap")}>
+                  Un evento para unir a la comunidad
+                </td>
+                <td className={clsx("text-[14px] text-blue-gray-600 font-normal px-6 py-4 whitespace-nowrap")}>
+                  17/01/2023
+                </td>
+                <td className={clsx("hidden lg:table-cell text-[14px] text-blue-gray-600 font-normal px-6 py-4 whitespace-nowrap")}>
+                  13:00
+                </td>
+                <td className={clsx("text-[14px] text-blue-gray-600 font-normal px-6 py-4 whitespace-nowrap")}>
+                  U$125.00
+                </td>
+                <td className={clsx("text-[14px] text-blue-gray-600 font-normal px-6 py-4 whitespace-nowrap")}>
+                  200
+                </td>
+                <td className={clsx("text-[14px] text-blue-gray-600 font-normal px-6 py-4 whitespace-nowrap")}>
+                  ...
+                </td>
+              </TableEventsRow>
             </tbody>
           </table>
         </div>
--- pages/eventsli.jsx
+++ pages/eventsli.jsx
@@ -1,17 +1,28 @@
-import Navbar from "../components/Navbar";
-import LandingHero from "../components/LandingHero";
-import LandingBenefits from "../components/LandingBenefits";
-import LandingPlan from "../components/LandingPlan";
-import Footer from "../components/Footer";
+import clsx from "clsx";
+import Sidebar from "../components/Sidebar";
+import EventsList from "../components/EventsList";
 
-export default function LandingPage() {
+
+export default function EventsPageList() {
   return (
     <>
-      <Navbar />
-      <LandingHero />
-      <LandingBenefits id="benefits"/>
-      <LandingPlan />
-      <Footer />
+      <main className={clsx("w-screen h-screen p-5")}>
+        <section>
+          <div className={clsx(" flex")}>
+            <Sidebar />
+            <article className={clsx("bg-green-100 px-4 md:px-8 mt-24 md:mt-24 lg:mt-14 xl:mt-10")}>
+              <h3 className={clsx("bg-pink-100 font-montserrat font-bold lg:mt-10 xl:mt-0 text-blue-gray-900 text-[24px] leading-[30px]")}>
+                Todos tus eventos
+              </h3> 
+              <section>
+                <div className={clsx("")}>
+                  <EventsList />
+                </div>
+              </section>
+            </article>
+          </div>
+        </section>
+      </main>
     </>
   );
 }
