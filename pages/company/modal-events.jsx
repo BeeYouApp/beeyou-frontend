693b23c594a13f889c5063bce1b2ee3912184892
annie <any_prettier_3@hotmail.com>
2022-12-12 00:05:57 -0600

    finish events modal

--- pages/company/modal-events.jsx
+++ pages/company/modal-events.jsx
@@ -1,19 +1,14 @@
 import clsx from "clsx";
-import ModalEvents from "../";
+import ModalEvents from "../../components/ModalEvents";
 
 export default function ModalEventsBiz(){
   return(
     <>
         <main className={clsx("w-screen h-screen p-10")}>
-          <section className={clsx("inline-block w-full h-1/4 m-auto font-poppins max-lg:h-[250px]")}>
-            <article className={clsx("text-center text-3xl font-bold mb-4")}>
-            </article>
-            <article className={clsx("text-center text-[18px] font-normal")}>
-            </article>
+          <section className={clsx(" w-full m-auto font-poppins max-lg:h-[250px]")}>
+            
           </section>
-          <article className={clsx("inline-flex w-5/12 max-lg:w-full h-3/4 max-lg:h-[450px] justify-end pt-[32px] max-lg:flex max-lg:justify-center")}>
-          </article>
-          <article className={clsx("inline-flex w-7/12 max-lg:w-full h-3/4 3 max-lg:h-[620px] pl-[40px] pt-[32px] max-lg:flex max-lg:justify-center max-lg:pl-[0px]")}>
+          <article className={clsx("max-lg:w-full h-/4 max-lg:h-[620px] pl-[40px] pt-[32px] max-lg:flex max-lg:justify-center max-lg:pl-[0px]")}>
             <ModalEvents></ModalEvents>
           </article>
         </main>
