246e2960407b3b50f1394f38355b6e1f79f5726f
annie <any_prettier_3@hotmail.com>
2022-12-12 19:33:19 -0600

    update events second part

---
+++ pages/company/events-update.jsx
@@ -0,0 +1,26 @@
+import clsx from "clsx";
+import UpdateEvents from "../../components/UpdateEvents";
+
+
+export default function UpdateEvent() {
+  return (
+    <>
+      <main className={clsx("w-screen h-screen p-5")}>
+        <section>
+          <div className={clsx("flex")}>
+            <article className={clsx("bg-green-100 px-4 md:px-8 mt-24 md:mt-24 lg:mt-14 xl:mt-10")}>
+              <h3 className={clsx("bg-pink-100 font-montserrat font-bold lg:mt-10 xl:mt-0 text-blue-gray-900 text-[24px] leading-[30px]")}>
+                Todos tus eventos
+              </h3> 
+              <section>
+                <div className={clsx("")}>
+                  <UpdateEvents />
+                </div>
+              </section>
+            </article>
+          </div>
+        </section>
+      </main>
+    </>
+  );
+}
