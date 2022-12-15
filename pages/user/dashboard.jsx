f4037a52ad6a45248e62d37769c470d46ea6b274
Víctor Cosme <victorcosmema@gmail.com>
2022-12-15 14:20:50 -0600

    Add Â'Cerca de ti' to dashboard

--- pages/user/dashboard.jsx
+++ pages/user/dashboard.jsx
@@ -1,3 +1,4 @@
+import clsx from "clsx";
 import Map from "../../components/Map";
 import { useRouter } from "next/router";
 import Button from "../../components/Button";
@@ -6,44 +7,49 @@ import CardCompany from "../../components/CardCompany";
 
 export default function Dashboard() {
   const router = useRouter();
-  if (typeof window !== 'undefined') {
+  if (typeof window !== "undefined") {
     if (localStorage.getItem("token")) {
       return (
-          <Layout>
-                <section className="w-11/12 radaial-2 m-auto rounded-2xl ">
-                  {/* <h1 className="text-4xl font-montserrat font-bold mb-10 mt-8">
-                    Espacios friendly
-                  </h1> */}
-                  {/* <Map></Map> */}
-                </section>
-                {/* <section className="w-11/12 m-auto mt-20 flex justify-between md:justify-around mb-10 ">
-                  <Button
-                    label="Cafeterías"
-                    style="bg-gray-700 w-96 rounded-full"
-                  ></Button>
-                  <Button
-                    label="Restaurantes"
-                    style="bg-gray-700 w-96 rounded-full"
-                  ></Button>
-                  <Button
-                    label="Bares"
-                    style="bg-gray-700 w-96 rounded-full"
-                  ></Button>
-                  <Button
-                    label="Librerías"
-                    style="bg-gray-700 w-96 rounded-full"
-                  ></Button>
-                  <Button
-                    label="Clubs nocturnos"
-                    style="bg-gray-700 w-96 rounded-full max-lg:hidden"
-                  ></Button>
-                  <Button
-                    label="Descuentos"
-                    style="bg-gray-700 w-96 rounded-full max-lg:hidden"
-                  ></Button>
-                </section> */}
-                {/* <CardCompany></CardCompany> */}
-          </Layout>
+        <Layout>
+          <section className={clsx("w-11/12 radaial-2 m-auto rounded-2xl")}>
+            <section className={clsx("flex items-center mt-8 xl:-mt-4")}>
+              <h1 className={clsx("text-[28px] leading-[36px] font-montserrat font-bold mb-10 lg:mt-0 to-blue-gray-800")}>
+                Espacios friendly
+              </h1>
+              <p className={clsx("font-montserrat font-bold text-[#FFFFFF] text-[12px] leading-4 bg-green-900 rounded-lg ml-3 px-3 py-1")}>
+                  Cerca de ti
+              </p>
+            </section>
+            <Map></Map>
+          </section>
+          <section className={clsx("w-11/12 m-auto mt-20 flex justify-between md:justify-around mb-10")}>
+            <Button
+              label="Cafeterías"
+              style="bg-gray-700 w-96 rounded-full">
+            </Button>
+            <Button
+              label="Restaurantes"
+              style="bg-gray-700 w-96 rounded-full">
+            </Button>
+            <Button
+              label="Bares"
+              style="bg-gray-700 w-96 rounded-full">
+            </Button>
+            <Button
+              label="Librerías"
+              style="bg-gray-700 w-96 rounded-full">
+            </Button>
+            <Button
+              label="Clubs nocturnos"
+              style="bg-gray-700 w-96 rounded-full max-lg:hidden">
+            </Button>
+            <Button
+              label="Descuentos"
+              style="bg-gray-700 w-96 rounded-full max-lg:hidden">
+            </Button>
+          </section>
+          <CardCompany></CardCompany>
+        </Layout>
       );
     } else {
       router.push(`/login`);
