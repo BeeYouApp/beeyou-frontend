9e3f17c3ef618342832296691d72570e0fa5da72
Estefanny Leyva <fanniluley88@gmail.com>
2022-12-27 20:52:18 -0600

    changes in to feed companies

--- components/FeedCompanies.jsx
+++ components/FeedCompanies.jsx
@@ -1,92 +1,42 @@
-import { getCompanies } from "../services/company.js"
 import React, { useEffect, useState, useContext } from "react";
-import clsx from "clsx";
-import { Navigation, Pagination, Scrollbar, A11y } from "swiper";
-import { Swiper, SwiperSlide } from "swiper/react";
-import "swiper/css";
-import "swiper/css/navigation";
-import "swiper/css/pagination";
-import "swiper/css/scrollbar";
-import "swiper/css/navigation";
-import Slider from "react-slick";
-import 'slick-carousel/slick/slick.css'
-import 'slick-carousel/slick/slick-theme.css'
 import dynamic from "next/dynamic";
-
+import { getCompanies } from "../services/company.js"
 const CardCompany = dynamic(() => import('./CardCompany'), {
-    ssr: false,
+  ssr: false,
 });
 
 export default function Feed() {
-    const [companies, setCompanies] = useState([]);
+  const [companies, setCompanies] = useState([]);
 
-    const getAllCompanies = async () => {
-        try {
-            const token = localStorage.getItem("token");
-            const response = await getCompanies(token)
-            const dataJson = await response.json()
-            setCompanies(dataJson.data.company)
-        } catch (error) {
-            console.log('Error: ', error)
-        }
+  const getAllCompanies = async () => {
+    try {
+      const token = localStorage.getItem("token");
+      const response = await getCompanies(token)
+      const dataJson = await response.json()
+      setCompanies(dataJson.data.company)
+    } catch (error) {
+      console.log('Error: ', error)
     }
-    useEffect(() => {
-        getAllCompanies()
-    },
-        [])
+  }
+  useEffect(() => {
+    getAllCompanies()
+  },
+    [])
 
-    const settings = {
-        dots: true,
-        infinite: true,
-        slidesToShow: 3,
-        slidesToScroll: 1,
-        speed: 500,
-        initialSlide: 0,
-        nextArrow: ">",
-        prevArrow: "<",
-        responsive: [
-            {
-                breakpoint: 992,
-                settings: {
-                    slidesToShow: 2,
-                    slidesToScroll: 2,
-                    infinite: true,
-                    dots: false
-                }
-            },
-            {
-                breakpoint: 768,
-                settings: {
-                    slidesToShow: 2,
-                    slidesToScroll: 2
-                }
-            },
-            {
-                breakpoint: 576,
-                settings: {
-                    slidesToShow: 1,
-                    slidesToScroll: 1
-                }
-            }
-        ]
-    };
-
-    return (
-        <div className="w-[100%]">
-            <Slider {...settings}>
-                {companies?.map((company, index) => (
-                    <div key={index}>
-                        <CardCompany
-                            title={company.brandName}
-                            image={company.image}
-                            stars="5"
-                            description={company.description}
-                            TagSection={company.type}
-                            discounts="ejemplo de descuento"
-                        />
-                    </div>
-                ))}
-            </Slider>
+  return (
+    <div className="w-[100%]">
+      {companies?.map((company, index) => (
+        <div key={index}>
+          <CardCompany
+            title={company.brandName}
+            image={company.image}
+            stars="5"
+            description={company.description}
+            TagSection={company.type}
+            discounts="ejemplo de descuento"
+          />
         </div>
-    );
+      ))}
+    </div>
+  );
 };
