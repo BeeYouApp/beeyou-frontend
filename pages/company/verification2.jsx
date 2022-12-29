f8f55d66ef8b0e783e915a06821adabf97d73fe2
annie <any_prettier_3@hotmail.com>
2022-12-28 22:06:51 -0600

    new try to get a flag

--- pages/company/verification2.jsx
+++ pages/company/verification2.jsx
@@ -2,24 +2,24 @@ import Navbar from "../../components/Navbar";
 import clsx from "clsx";
 import VerifyBiz from "../../components/VerifyBiz";
 
-export default function Validation2(){
-  return(
-  <>
-    <Navbar/>
-      <main className={clsx("w-screen h-screen")}>
-        <section className={clsx("inline-block w-full h-1/4 m-auto font-poppins max-lg:h-[250px]")}>
-          <article className={clsx("text-center text-3xl font-bold mb-4")}>
-            <h1 className={clsx("mt-20 text-blue-gray-900 font-poppins")}>Verifica tu negocio como</h1>
-            <h1 className={clsx("lgbtiq-grad-color leading-tight")}>bee you friendly!</h1>
-          </article>
-          <article className={clsx("text-center text-[18px] font-normal")}>
-            <h3>Marca las casillas que cumplen para continuar</h3>
-          </article>
-        </section>
-        <section className={clsx("flex justify-center")}>
-          <VerifyBiz></VerifyBiz>
-        </section>
-      </main>
-  </>
-  )
+export default function Validation2() {
+  // return(
+  // <>
+  //   <Navbar/>
+  //     <main className={clsx("w-screen h-screen")}>
+  //       <section className={clsx("inline-block w-full h-1/4 m-auto font-poppins max-lg:h-[250px]")}>
+  //         <article className={clsx("text-center text-3xl font-bold mb-4")}>
+  //           <h1 className={clsx("mt-20 text-blue-gray-900 font-poppins")}>Verifica tu negocio como</h1>
+  //           <h1 className={clsx("lgbtiq-grad-color leading-tight")}>bee you friendly!</h1>
+  //         </article>
+  //         <article className={clsx("text-center text-[18px] font-normal")}>
+  //           <h3>Marca las casillas que cumplen para continuar</h3>
+  //         </article>
+  //       </section>
+  //       <section className={clsx("flex justify-center")}>
+  //         <VerifyBiz></VerifyBiz>
+  //       </section>
+  //     </main>
+  // </>
+  // )
 }
